d15277447c1685c2c1022b95cd06804429818e7e
hanting001 <hanting001@hotmail.com>
2017-12-07 21:49:00 +0800

    add HD key

--- server/router/account.js
+++ server/router/account.js
@@ -1,18 +1,20 @@
 const Mnemonic = require('bitcore-mnemonic');
-// const code = new Mnemonic('才 有 劝 攻 佳 驶 塑 妙 壤 迁 看 繁');
-const code = new Mnemonic();
+
 
 const Web3 = require('../lib/web3');
 
 module.exports = (server) => {
     server.get('/account/new', async(req, res, next) => {
-        const password = 'alfjahhljalf';
+        let password = 'alfjahhljalf';
+        // const code = new Mnemonic('才 有 劝 攻 佳 驶 塑 妙 壤 迁 看 繁');
+        const code = new Mnemonic(Mnemonic.Words.CHINESE);
         const web3 = Web3.instance();
         // console.log(web3.eth.accounts.wallet);
         let mnemonic = code.toString();
         // console.log(mnemonic);
-        var xpriv = code.toHDPrivateKey(password + '1');
-        let account = web3.eth.accounts.privateKeyToAccount(web3.utils.toHex(xpriv.toString()));
+        var parent = code.toHDPrivateKey();
+        console.log(parent.deriveChild(0));
+        let account = web3.eth.accounts.privateKeyToAccount(web3.utils.toHex(parent.toString()));
         const keystore = account.encrypt(password);
         const acc = web3.eth.accounts.decrypt(keystore, password);
         console.log(account.address);
