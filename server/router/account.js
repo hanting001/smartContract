b37bd34d4171b4502df04b490a490ca62404142f
hanting001 <hanting001@hotmail.com>
2017-12-07 19:21:15 +0800

    增加HD Private Key

--- server/router/account.js
+++ server/router/account.js
@@ -1,16 +1,24 @@
+const Mnemonic = require('bitcore-mnemonic');
+// const code = new Mnemonic('才 有 劝 攻 佳 驶 塑 妙 壤 迁 看 繁');
+const code = new Mnemonic();
+
 const Web3 = require('../lib/web3');
 
 module.exports = (server) => {
     server.get('/account/new', async(req, res, next) => {
+        const password = 'alfjahhljalf';
         const web3 = Web3.instance();
         // console.log(web3.eth.accounts.wallet);
-        let account = web3.eth.accounts.create();
-        const keystore = account.encrypt('alfjahhljalf');
-        const acc = web3.eth.accounts.decrypt(keystore, 'alfjahhljalf');
-        console.log(account);
-        console.log(keystore);
-        console.log(acc);
-        res.send('OK');
+        let mnemonic = code.toString();
+        // console.log(mnemonic);
+        var xpriv = code.toHDPrivateKey(password + '1');
+        let account = web3.eth.accounts.privateKeyToAccount(web3.utils.toHex(xpriv.toString()));
+        const keystore = account.encrypt(password);
+        const acc = web3.eth.accounts.decrypt(keystore, password);
+        console.log(account.address);
+        // console.log(keystore);
+        // console.log(acc);
+        res.send('一定要记住：' + mnemonic);
         next();
     });
 }
