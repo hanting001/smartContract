49a98ce3d8e7d0cacf1f165e433b005107667d1d
hanting001 <hanting001@hotmail.com>
2017-12-08 11:58:43 +0800

    add audit

--- server/lib/init.js
+++ server/lib/init.js
@@ -1,11 +1,20 @@
 'use strict';
 const restify = require('restify');
+const bunyan = require('bunyan');
 
 const conf = require('./config');
 const web3 = require('./web3');
 
 module.exports = (server) => {
     server.use(restify.plugins.bodyParser());
+    server.on('after', restify.plugins.auditLogger({
+        log: bunyan.createLogger({
+            name: 'audit',
+            stream: process.stdout
+        }),
+        printLog: true,
+        event: 'after'
+    }));
     server.use(conf.middleware());
     web3.init(conf);
 }
