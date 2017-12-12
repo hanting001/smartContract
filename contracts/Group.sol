62ba604dbfe95f2e1024015cd87f4a4348e6fa70
hanting001 <hanting001@hotmail.com>
2017-12-12 14:56:05 +0800

    add Group smartContract

--- contracts/Group.sol
+++ contracts/Group.sol
@@ -1,13 +1,31 @@
 pragma solidity ^0.4.18;
 
+import '../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol';
 import './common/Stoppable.sol';
 
-contract Group is Stoppable {
+contract Group is Stoppable,  Ownable {
     address[] members;
     string item;
     bool isOpen;
-    function Group(address _curator) public
-        Stoppable(_curator) {
+
+    function Group() public
+        Stoppable(msg.sender) {
         // constructor
+    }
+
+    /**
+    * @dev Throws if group is not open.
+    */
+    modifier onlyOpen() {
+        require(isOpen);
+        _;
+    }
+
+    //manage
+    function open() onlyOwner public  {
+        isOpen = true;
+    }
+    function close() onlyOwner public {
+        isOpen = false;
     }
 }
