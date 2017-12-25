8dac8e6e7ccfc94fb64f9fa321864a27f6eafcbb
hanting001 <hanting001@hotmail.com>
2017-12-25 16:26:41 +0800

    deploy by self

---
+++ migrations/my_depoly_token.js
@@ -0,0 +1,23 @@
+const Web3 = require('web3');
+const KnotToken = require('../build/contracts/KnotToken');
+
+const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:9545'));
+
+let account = web3.eth.accounts[0];
+let args = process.argv;
+
+if (args[2]) {
+    web3.personal.unlockAccount(account, args[2], web3.toHex(15000));
+}
+
+const code = KnotToken.bytecode;
+
+const token = new web3.eth.Contract(KnotToken.api);
+
+const deploy = async function () {
+    const obj = token.deploy({
+        data: code
+    });
+    console.log(obj);
+}
