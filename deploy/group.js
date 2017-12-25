f7724ef01f99d46699e11040121810120d8e4406
hanting001 <hanting001@hotmail.com>
2017-12-25 17:54:16 +0800

    fix bugs

--- deploy/group.js
+++ deploy/group.js
@@ -1,9 +1,12 @@
 const utility = require('../migrations/utility');
 const args = process.argv;
 
-if (process.env.NODE_ENV == 'test') {
+
+if (process.env.NODE_ENV == 'test' && args.slice(3).length > 1) {
     (async() => {
         const address = await utility.deploy('Group', args[2], args.slice(3));
-        utility.updateDB('G00000001', address);
+        utility.updateDB(args[3], address);
     })();
+} else {
+    console.log('参数错误，正确：node group 密码 groupItemName tokenAddress')
 }
