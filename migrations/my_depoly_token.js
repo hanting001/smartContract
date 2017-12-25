af98dd01fd3a4d0b543722af211c4b8b590eb327
hanting001 <hanting001@hotmail.com>
2017-12-25 17:26:10 +0800

    fix bugs

--- migrations/my_depoly_token.js
+++ migrations/my_depoly_token.js
@@ -2,5 +2,7 @@ const utility = require('./utility');
 const args = process.argv;
 
 
-const address = await utility.deploy('knotCoin', args[2]);
-utility.updateDB('knotCoin', address);
+(async() => {
+    const address = await utility.deploy('knotCoin', args[2]);
+    utility.updateDB('knotCoin', address);
+})();
