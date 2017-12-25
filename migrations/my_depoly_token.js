02aba3466e00f6809b30408ef61a42ebfc186d65
hanting001 <hanting001@hotmail.com>
2017-12-25 17:23:48 +0800

    fix bugs

--- migrations/my_depoly_token.js
+++ migrations/my_depoly_token.js
@@ -1,32 +1,6 @@
-const Web3 = require('web3');
-
 const utility = require('./utility');
-const KnotToken = require('../build/contracts/KnotToken');
-
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:9545'));
 const args = process.argv;
-const code = KnotToken.bytecode;
 
-const token = new web3.eth.Contract(KnotToken.abi);
 
-const deploy = async function () {
-    const accounts = await web3.eth.getAccounts();
-    const account = accounts[0];
-    console.log(account);
-    if (args[2]) {
-        web3.eth.personal.unlockAccount(account, args[2], web3.utils.toHex(15000));
-    }
-    const obj = token.deploy({
-        data: code
-    });
-    const gas = await obj.estimateGas();
-    const newContractInstance = await obj.send({
-        from: account,
-        gasLimit: gas * 2
-    });
-    console.log(newContractInstance.options.address);
-    utility.updateDB('knotCoin', newContractInstance.options.address);
-}
-
-deploy();
+const address = await utility.deploy('knotCoin', args[2]);
+utility.updateDB('knotCoin', address);
--- migrations/utility.js
+++ migrations/utility.js
@@ -1,4 +1,7 @@
 const request = require('request-promise-native');
+const Web3 = require('web3');
+const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider(conf.get('httpProvider')));
 
 const conf = require('../server/lib/config');
 
@@ -16,5 +19,28 @@ module.exports = {
             json: true
         };
         return request(options);
+    },
+    deploy: async(contract, password, params) => {
+        const contractJson = require('../build/contracts/' + contract);
+        const contractObj = new web3.eth.Contract(contractJson.abi);
+        const code = contractJson.bytecode;
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
+        if (password) {//password
+            web3.eth.personal.unlockAccount(account, password, web3.utils.toHex(15000));
+        }
+        let options = {
+            data: code
+        };
+        if (params) {
+            options.arguments = params;
+        }
+        const obj = contractObj.deploy(options);
+        const gas = await obj.estimateGas();
+        const newContractInstance = await obj.send({
+            from: account,
+            gasLimit: gas * 2
+        });
+        return newContractInstance.options.address;
     }
 }
