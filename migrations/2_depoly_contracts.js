3ae4ce328fd2243c47f0c2f3eef05e6b45f06540
hanting001 <hanting001@hotmail.com>
2017-12-26 09:29:24 +0800

    开发环境每次都重新部署合约

--- migrations/2_depoly_contracts.js
+++ migrations/2_depoly_contracts.js
@@ -6,19 +6,19 @@ const Group = artifacts.require('Group');
 module.exports = async(deployer, network) => {
     const args = process.argv;
     try {
-        if (!KnotCoin.address && args.indexOf('knotCoin') > 0) {
+        if (!KnotCoin.address) {
             await deployer.deploy(KnotCoin);
             utility.updateDB('knotCoin', KnotCoin.address);
         }
     } catch (err) {
-        if (args.indexOf('knotCoin') > 0) {
+        // if (args.indexOf('knotCoin') > 0) {
             await deployer.deploy(KnotCoin);
             utility.updateDB('knotCoin', KnotCoin.address);
-        }
+        // }
     }
-    if (args.indexOf('group') > 0) {
+    // if (args.indexOf('group') > 0) {
         await deployer.deploy(Group, 'G00000001', KnotCoin.address);
         utility.updateDB('G00000001', Group.address);
-    }
+    // }
 
 }
