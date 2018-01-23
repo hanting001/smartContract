fbb5a682c7b1c233484ee93f60d5e193c9373480
hanting001 <hanting001@hotmail.com>
2018-01-23 15:59:31 +0800

    add oracle

---
+++ deploy/delayOracle.js
@@ -0,0 +1,12 @@
+const utility = require('../migrations/utility');
+const args = process.argv;
+
+
+if (process.env.NODE_ENV == 'test') {
+    (async() => {
+        const address = await utility.deploy('DelayOracle', args[2]);
+        utility.updateDB(args[3], address);
+    })();
+} else {
+    console.log('参数错误，正确：node delayOracle 密码')
+}
