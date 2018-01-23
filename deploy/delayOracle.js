e911379c1098b5777eef4ac6e79014bd48109600
hanting001 <hanting001@hotmail.com>
2018-01-23 16:08:53 +0800

    fix bugs

--- deploy/delayOracle.js
+++ deploy/delayOracle.js
@@ -5,7 +5,7 @@ const args = process.argv;
 if (process.env.NODE_ENV == 'test') {
     (async() => {
         const address = await utility.deploy('DelayOracle', args[2]);
-        utility.updateDB(args[3], address);
+        utility.updateDB('delayOracle', address);
     })();
 } else {
     console.log('参数错误，正确：node delayOracle 密码')
