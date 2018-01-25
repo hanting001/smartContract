aaddfe18efeaa3a6436ac7eb38bce876b7ded9b9
hanting001 <hanting001@hotmail.com>
2018-01-25 12:29:39 +0800

    test oracle

--- contracts/common/DelayOracle.sol
+++ contracts/common/DelayOracle.sol
@@ -33,7 +33,7 @@ contract DelayOracle is usingOraclize {
     }
 
     function query(string flightNo, string flightDate) public payable {
-        // require(this.balance > oraclize_getPrice("URL"));
+        require(this.balance > oraclize_getPrice("URL"));
         
         
         string memory a = "json(http://op.juhe.cn/flight/df/hfs?dtype=&flightNo=";
