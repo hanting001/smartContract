f92553e4501a5ae68273b3116e89ee43f97b1279
hanting001 <hanting001@hotmail.com>
2018-01-25 16:26:23 +0800

    test oracle

--- contracts/common/DelayOracle.sol
+++ contracts/common/DelayOracle.sol
@@ -33,8 +33,10 @@ contract DelayOracle is usingOraclize {
         var s = result.toSlice();
         var d = s.split(", ".toSlice());
         d = d.beyond("[\"".toSlice());
+        d = d.until("\"]".toSlice());
         string memory _arrScheduled = d.toString();
-        string memory _arrActual = s.until("\"]".toSlice()).toString();
+        s = s.until("\"]".toSlice());
+        string memory _arrActual = s.beyond("[\"".toSlice()).toString();
         results[keccak256(queryRecords[queryId].record)] = Info({arrScheduled: _arrScheduled, arrActual: _arrActual});
         LogDelayInfoUpdated(queryRecords[queryId].record);
         
