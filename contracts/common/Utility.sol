cd2f11b27243d203cadb4878717646d723f46ddc
hanting001 <hanting001@hotmail.com>
2018-01-30 17:22:16 +0800

    add library

--- contracts/common/Utility.sol
+++ contracts/common/Utility.sol
@@ -3,8 +3,11 @@ import "../../installed_contracts/solidity-stringutils/strings.sol";
 
 library Utility {
     using strings for *;
-    function getDatetime(string scheduled) internal returns (uint) {
-        var a = scheduled.toSlice();
+    /** @dev getDatetime 
+      * @param dateTimeStr 日期字符串，格式为"2018-01-30T17:23:15"
+      */   
+    function getDatetime(string dateTimeStr) internal returns (uint) {
+        var a = dateTimeStr.toSlice();
         var aDate = a.split("T".toSlice());
         var aTime = a;
         strings.slice memory part;
@@ -22,13 +25,43 @@ library Utility {
         uint aIntYear = parseInt(year);
         uint aIntMonth = parseInt(month);
         uint aIntDay = parseInt(day);
-        return aIntYear * 1 years + aIntMonth * 30 days  + aIntDay * 1 days;
+        uint monthDays = 0;
+        for (uint i = 1; i <= aIntMonth; i ++) {
+            monthDays += getMonthDays(aIntYear, i);
+        }
+        return aIntYear * 1 years + monthDays * 1 days  + aIntDay * 1 days;
     }
     function getTime(string hour, string minute, string second) internal pure returns (uint) {
         uint aIntHour = parseInt(hour);
         uint aIntMinute = parseInt(minute);
         uint aIntSecond = parseInt(second);
         return aIntHour * 1 hours + aIntMinute * 1 minutes + aIntSecond;
+    }
+
+    function isLeapYear(uint year) public pure returns (bool) {
+                if (year % 4 != 0) {
+                        return false;
+                }
+                if (year % 100 != 0) {
+                        return true;
+                }
+                if (year % 400 != 0) {
+                        return false;
+                }
+                return true;
+    }
+    function getMonthDays(uint year, uint month) public pure returns (uint) {
+        if (month == 2) {
+            if (isLeapYear(year)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        }
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            return 31;
+        }
+        return 30;
     }
     function parseInt(string _a) internal pure returns (uint) {
         return parseInt(_a, 0);
