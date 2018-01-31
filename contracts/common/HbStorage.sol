d77b0de82428f52bb1da9903e5a5293de74b082d
hanting001 <hanting001@hotmail.com>
2018-01-31 11:03:00 +0800

    add db contruct

--- contracts/common/HbStorage.sol
+++ contracts/common/HbStorage.sol
@@ -3,21 +3,50 @@ pragma solidity ^0.4.18;
 import '../../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract HbStorage is Ownable {
-    mapping(string => mapping(address => bool)) address2bool;
-    mapping(string => address[])  addressArray;
+    enum SFStatus { opening, closed, claiming, ended }
+    struct SFInfo {
+        address[] members;
+        SFStatus status;
+        bool isDelayed;
+        bool isValued;
+    }
+    struct SFHistroy {
+        uint totalCounts;
+        uint delayedCounts;
+    }
+    struct VotedMember {
+        address member;
+        bool vote;
+        bool isValued;
+    }
+    struct VoteInfo {
+        VotedMember[] votedMembers;
+        uint yesCounts;
+        uint noCounts;
+        bool isValued;
+    }
+    struct MemberSF {
+        bytes32 joinSF;
+        bytes32 votedSF;
+        bool isValued;
+    }
+    struct MemberInfo {
+        MemberSF[] scheduledFlights;
+        uint winCounts;
+    }
+
+    //已出单航班信息 {keccak256(航班号+日期): [SFInfo]}
+    mapping(bytes32 => SFInfo[]) scheduledFlights;
+    //记录航班的投票信息 {keccak256(航班号+日期): [VoteInfo]}
+    mapping(bytes32 => VoteInfo[])  votes;
+    //记录航班的历史延误信息 {keccak256(航班号+日期): SFHistroy}
+    mapping(bytes32 => SFHistroy) sfHistroy;
+    //记录用户的航班信息
+    mapping(address => MemberInfo) memberInfos;
+
+    
     function HbStorage() public {
         // constructor
     }
-    function getBoolByAddress(string key, address by) public view returns(bool) {
-        return address2bool[key][by];
-    }
-    function setBoolByAddress(string key, address by, bool _bool) public {
-        address2bool[key][by] = _bool;
-    }
-    function getAddressArrayByIndex(string key, uint index) public view returns(address) {
-        return addressArray[key][index];
-    }
-    function setAddressArray(string key, address _address) public {
-        addressArray[key].push(_address);
-    }
+    
 }
