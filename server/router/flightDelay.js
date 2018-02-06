43e8e0bc908eb6d13f5acd2acabe45c36ed2a2de
hanting001 <hanting001@hotmail.com>
2018-02-06 14:12:36 +0800

    hbStorage add event

--- server/router/flightDelay.js
+++ server/router/flightDelay.js
@@ -1,6 +1,25 @@
+const myWeb3 = require('../lib/web3');
+const errors = require('restify-errors');
+const auth = require('../lib/auth');
 
+const FlightDelay = require('../contracts/FlightDelay');
 
 module.exports = (server) => {
     this.path = '/flightDelay';
-
+    server.get(this.path + '/getSFInfo/:flightNO/:flightDate', auth.jwt, async(req, res, next) => {
+        try {
+            const flightNO = req.params.flightNO;
+            const flightDate = req.params.flightDate;
+            const flightDelaySC = await FlightDelay.instance();
+            const result = await flightDelaySC.getSFInfo(flightNO, flightDate, req.user.account);
+            
+            res.send({
+                output: result
+            });
+            next();
+        } catch (err) {
+            console.log(err);
+            next(new errors.InternalServerError(err));
+        }
+    });
 };
