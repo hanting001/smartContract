ed847c3c92da33afc10ecdced4c19230ed32e36b
Macx <Macx@MacxdeMacBook-Pro-584.local>
2018-03-19 17:05:09 +0800

    loading模块增加

--- flightDelayClient/src/app/home/home.component.ts
+++ flightDelayClient/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { LoadingService } from '../service/loading.service';
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Web3Service, FlightDelayService } from '../service/index';
@@ -7,74 +8,84 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  account: string;
-  sfInfo: string;
-  winHeight: any;
-  form: FormGroup;
-  minDate: Date;
-  modalRef: BsModalRef;
-  confirmMessage: string;
-  @ViewChild('confirmTemplate') confirmTemplate: TemplateRef<any>;
-  constructor(private fb: FormBuilder, private web3: Web3Service,
-    private flightDelayService: FlightDelayService, private localService: BsLocaleService,
-    private modalService: BsModalService,
-    private router: Router) {
-    this.form = this.fb.group({
-      flightNO: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]{2}[0-9]{4}$/)]],
-      flightDate: ['', [Validators.required]]
-    });
-  }
+    account: string;
+    sfInfo: string;
+    winHeight: any;
+    form: FormGroup;
+    minDate: Date;
+    modalRef: BsModalRef;
+    confirmMessage: string;
 
-  ngOnInit() {
-    this.web3.getMainAccount().then(account => {
-      this.account = account;
-    });
-    // 测试
-    this.flightDelayService.getCurrentVote();
-    this.localService.use('zh-cn');
-    this.minDate = new Date();
-    this.minDate.setDate(this.minDate.getDate() + 1);
-  }
-  async sendTx() {
-    const confirmApprove = async (confirmationNumber, receipt) => {
-      if (confirmationNumber === 2) {
-        const result = await this.flightDelayService.getSFInfo('SF5050', '2018-03-09');
-        this.sfInfo = JSON.stringify(result);
-      }
-    };
-    this.flightDelayService.setMaxCount(Math.random() * 100, confirmApprove);
-  }
-  async join() {
-    if (this.form.valid) {
-      const model = this.form.value;
-      const currentVote = await this.flightDelayService.getCurrentVote();
-      const balance = await this.flightDelayService.getBalance();
-      const price = await this.flightDelayService.getPrice(model.flightNO);
-      console.log(balance);
-      console.log(price);
-      if (balance.token < price) {
-        this.confirmMessage = `token余额不足${price}，是否前往兑换？`;
-        this.openModal(this.confirmTemplate);
-      }
-      // this.router.navigate(['/']);
+
+    @ViewChild('confirmTemplate') confirmTemplate: TemplateRef<any>;
+    constructor(private fb: FormBuilder, private web3: Web3Service,
+        private flightDelayService: FlightDelayService, private localService: BsLocaleService,
+        private modalService: BsModalService,
+        private router: Router, public loadingSer: LoadingService) {
+        this.form = this.fb.group({
+            flightNO: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]{2}[0-9]{4}$/)]],
+            flightDate: ['', [Validators.required]]
+        });
+
     }
-    const flightDate = this.form.get('flightDate');
-  }
 
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
-  }
-  decline() {
-    this.modalRef.hide();
-  }
-  goExchange() {
-    this.router.navigate(['/exchange']);
-  }
-  get flightNO() { return this.form.get('flightNO'); }
-  get flightDate() { return this.form.get('flightDate'); }
+    ngOnInit() {
+        this.web3.getMainAccount().then(account => {
+            this.account = account;
+        });
+        // 测试
+        this.flightDelayService.getCurrentVote();
+        this.localService.use('zh-cn');
+        this.minDate = new Date();
+        this.minDate.setDate(this.minDate.getDate() + 1);
+    }
+    async sendTx() {
+        const confirmApprove = async (confirmationNumber, receipt) => {
+            if (confirmationNumber === 2) {
+                const result = await this.flightDelayService.getSFInfo('SF5050', '2018-03-09');
+                this.sfInfo = JSON.stringify(result);
+            }
+        };
+        this.flightDelayService.setMaxCount(Math.random() * 100, confirmApprove);
+    }
+    async join() {
+
+        if (this.form.valid) {
+            this.loadingSer.show();
+            const model = this.form.value;
+            const currentVote = await this.flightDelayService.getCurrentVote();
+            const balance = await this.flightDelayService.getBalance();
+            const price = await this.flightDelayService.getPrice(model.flightNO);
+            console.log(balance);
+            console.log(price);
+            if (balance.token < price) {
+                this.confirmMessage = `token余额不足${price}，是否前往兑换？`;
+                this.openModal(this.confirmTemplate);
+                this.loadingSer.hide();
+            }
+            // this.router.navigate(['/']);
+        }
+        const flightDate = this.form.get('flightDate');
+    }
+
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
+    }
+    decline() {
+        this.modalRef.hide();
+    }
+    goExchange() {
+        this.router.navigate(['/exchange']);
+        this.modalRef.hide();
+    }
+
+
+
+    get flightNO() { return this.form.get('flightNO'); }
+    get flightDate() { return this.form.get('flightDate'); }
 }
--- flightDelayClient/src/app/service/index.ts
+++ flightDelayClient/src/app/service/index.ts
@@ -1,2 +1,3 @@
 export { Web3Service } from './web3.service';
 export { FlightDelayService } from './flight-delay.service';
+export { LoadingService } from './loading.service';
