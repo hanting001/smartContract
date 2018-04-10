834c481b5cac4ac0dc465ab2f31eca675df1d52b
Macx <Macx@MacxdeMacBook-Pro-584.local>
2018-04-10 09:47:57 +0800

    优化

--- worldCupClient/src/app/service/index.ts
+++ worldCupClient/src/app/service/index.ts
@@ -1,4 +1,3 @@
-import { OrderComponent } from '../order/order.component';
 export { Web3Service } from './web3.service';
 export { FlightDelayService } from './flight-delay.service';
 export { LoadingService } from './loading.service';
