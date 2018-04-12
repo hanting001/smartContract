50f83d61f570a6e40d564bd45f2b38fb14243b5e
Macx <Macx@MacxdeMacBook-Pro-584.local>
2018-04-12 16:56:13 +0800

    自定义

--- worldCupClient/src/app/fifa/chart/chart.component.ts
+++ worldCupClient/src/app/fifa/chart/chart.component.ts
@@ -47,31 +47,108 @@ export class ChartComponent implements OnInit, AfterViewInit {
         this.generateChart();
     }
 
+    customTooltips(tooltip) {
+        console.log(tooltip);
+        // Tooltip Element
+        let tooltipEl = document.getElementById('chartjs-tooltip');
+
+        if (!tooltipEl) {
+            tooltipEl = document.createElement('div');
+            tooltipEl.id = 'chartjs-tooltip';
+            tooltipEl.innerHTML = '<table></table>';
+            // this.chartRef.nativeElement.
+            document.body.appendChild(tooltipEl);
+        }
+
+        // Hide if no tooltip
+        if (tooltip.opacity === 0) {
+            tooltipEl.style.opacity = '0';
+            return;
+        }
+
+        // Set caret Position
+        tooltipEl.classList.remove('above', 'below', 'no-transform');
+        if (tooltip.yAlign) {
+            tooltipEl.classList.add(tooltip.yAlign);
+        } else {
+            tooltipEl.classList.add('no-transform');
+        }
+
+        function getBody(bodyItem) {
+            return bodyItem.lines;
+        }
+
+        // Set Text
+        if (tooltip.body) {
+            const titleLines = tooltip.title || [];
+            const bodyLines = tooltip.body.map(getBody);
+
+            let innerHtml = '<thead>';
+
+            titleLines.forEach(function (title) {
+                innerHtml += '<tr><th>' + title + '</th></tr>';
+            });
+            innerHtml += '</thead><tbody>';
+
+            bodyLines.forEach(function (body, i) {
+                const colors = tooltip.labelColors[i];
+                let style = 'background:' + colors.backgroundColor;
+                style += '; border-color:' + colors.borderColor;
+                style += '; border-width: 2px';
+                const span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+                innerHtml += '<tr><td>' + span + body + '</td></tr>';
+            });
+            innerHtml += '</tbody>';
+
+            const tableRoot = tooltipEl.querySelector('table');
+            tableRoot.innerHTML = innerHtml;
+        }
+
+        const positionY = document.getElementsByTagName('canvas')[0].offsetTop;
+        const positionX = document.getElementsByTagName('canvas')[0].offsetLeft;
+        console.log(positionX);
+        console.log(positionY);
+
+        // Display, position, and set styles for font
+        tooltipEl.style.opacity = '1';
+        tooltipEl.style.zIndex = '2000';
+        tooltipEl.style.position = 'absolute';
+        tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+        tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+        tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+        tooltipEl.style.fontSize = tooltip.bodyFontSize + 'px';
+        tooltipEl.style.fontStyle = tooltip._bodyFontStyle;
+        tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+    }
+
     generateChart() {
         this.validateChart();
 
         const charCtx = this.chartRef.nativeElement.getContext('2d');
         this.chart = new Chart(charCtx, {
-            animating: true,
             type: this.chartType,
             data: {
                 labels: this.chartLabels,
                 datasets: [{
                     label: this.dataSetLabel === undefined ? '' : this.dataSetLabel,
                     data: this.chartData,
-                    backgroundColor: this.generateRandomColors()
-                    ,
+                    backgroundColor: this.generateRandomColors(),
+                    fill: false
                 }]
             },
             options: {
+                responsive: true,
                 defaultColor: 'rgba(255, 159, 64, 0.2)',
                 title: {
                     fontSize: 22,
                     display: true,
                     text: this.chartTitle
                 },
-                tooltip: {
-                    enabled: true
+                tooltips: {
+                    enabled: false,
+                    position: 'nearest',
+                    mode: 'index',
+                    custom: this.customTooltips
                 },
                 legend: {
                     display: true,
@@ -89,9 +166,10 @@ export class ChartComponent implements OnInit, AfterViewInit {
                 },
 
                 onClick: (event) => {
-                    console.log(event)
-                    this.onClick.emit(event.target)
-                }
+                    console.log(event);
+                    this.onClick.emit(event.target);
+                },
+                fill: false
 
             }
         });
