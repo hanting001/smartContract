c447225649e6ea07fb2f783e35d4a19417beda4c
Macx <Macx@MacxdeMacBook-Pro-584.local>
2018-04-19 15:13:31 +0800

    修改

--- contracts/common/KnotToken.sol
+++ contracts/common/KnotToken.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.4.18;
 
-import '../../node_modules/zeppelin-solidity/contracts/token/StandardToken.sol';
+import '../../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
 contract KnotToken is StandardToken {
     string public name = 'KnotCoin';
     string public symbol = 'KTC';
+    uint public totalSupply = 0;
     uint public decimals = 18;
     uint public INITIAL_SUPPLY = 1000000000 * (10 ** decimals);
     
