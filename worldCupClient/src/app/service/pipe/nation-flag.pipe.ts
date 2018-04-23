119013af1128b1e33cd8a9cf127d65e691b2e6ee
hanting001 <hanting001@hotmail.com>
2018-04-23 17:10:02 +0800

    add

--- worldCupClient/src/app/service/pipe/nation-flag.pipe.ts
+++ worldCupClient/src/app/service/pipe/nation-flag.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Pipe({
     name: 'nationFlag'
 })
 export class NationFlagPipe implements PipeTransform {
+    constructor(private http: HttpClient) {
 
+    }
     transform(value: any, args?: any): any {
         value = String(value).toUpperCase();
+        if (value.length <= 3) {
+            return'assets/images/nation-flags/placeholder.png';
+        }
         return 'assets/images/nation-flags/' + value.replace(/[ ]/g, '') + '.png';
     }
 
