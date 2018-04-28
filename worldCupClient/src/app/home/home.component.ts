c1ba7097ed54854ed78844c848714e714b08da93
hanting001 <hanting001@hotmail.com>
2018-04-28 13:25:54 +0800

    fix bugs

--- worldCupClient/src/app/home/home.component.ts
+++ worldCupClient/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Web3Service } from '../service/index';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private web3: Web3Service) { }
 
   ngOnInit() {
+    this.web3.check();
   }
 
 }
