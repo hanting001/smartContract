91e08ca3cd634aac27d57a58e21cae08fa9db5a0
Macx <Macx@MacxdeMacBook-Pro-584.local>
2018-05-22 09:56:33 +0800

    修改

--- deploy/flightDelay.js
+++ deploy/flightDelay.js
@@ -4,8 +4,10 @@ const args = process.argv;
 
 if ('test' == 'test') {
     (async() => {
-        const address = await utility.deploy('FlightDelay', args[2], args.slice(3));
-        utility.updateDB('flightDelay', 'FlightDelay', address);
+        const address = await utility.deploy('FlightDelay', args[2], args.slice(3), (address) => {
+            utility.updateDB('flightDelay', 'FlightDelay', address);
+        });
+
     })();
 } else {
     console.log('参数错误，正确：node flightDelay 密码 hbAddress tokenAddress');
--- deploy/flightDelayService.js
+++ deploy/flightDelayService.js
@@ -4,8 +4,10 @@ const args = process.argv;
 
 if ('test' == 'test') {
     (async() => {
-        const address = await utility.deploy('FlightDelayService', args[2], args.slice(3));
-        utility.updateDB('flightDelayService', 'FlightDelayService', address);
+        const address = await utility.deploy('FlightDelayService', args[2], args.slice(3), (address) => {
+            utility.updateDB('flightDelayService', 'FlightDelayService', address);
+        });
+
     })();
 } else {
     console.log('参数错误，正确：node flightDelayService 密码 hbAddress tokenAddress');
